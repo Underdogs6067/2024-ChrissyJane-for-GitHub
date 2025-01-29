f8bed3630c4f0dcadb5923a747861ce9e4680770
Underdogs6067 <Team6067>
2025-01-29 16:25:52 -0500

    initial commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024 ChrissyJane for GitHub";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-01-29 16:08:07 EST";
+  public static final long BUILD_UNIX_TIME = 1738184887481L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
